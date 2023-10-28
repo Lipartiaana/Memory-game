3cb4394e977c6958ac9457f8ceb8f78d1c01e116
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2023-10-28 14:38:14 +0400

    initial commit

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const imagesArray = [
+  "./assets/1.jpeg",
+  "./assets/2.jpg",
+  "./assets/3.jpg",
+  "./assets/4.jpg",
+  "./assets/5.jpg",
+  "./assets/6.jpg",
+  "./assets/7.jpg",
+  "./assets/8.jpg",
+];
+
+export default imagesArray;
---
+++ js/script.js
@@ -0,0 +1,105 @@
+import imagesArray from "./data.js";
+
+const imageWrapper = document.querySelector(".img-wrapper");
+const scoreElem = document.getElementById("score");
+
+const cards = [];
+let flippedCards = [];
+let score = 0;
+let isChecking = false;
+
+imagesArray.forEach((img) => {
+  const card = document.createElement("div");
+  const card2 = document.createElement("div");
+  createCard(img, card);
+  createCard(img, card2);
+  cards.push(card, card2);
+});
+
+function createCard(img, card) {
+  const html = `<img src="${img}" alt="image">`;
+  card.innerHTML = html;
+  card.classList.add("card");
+  card.addEventListener("click", clickHandler);
+}
+
+function disableCardClicks() {
+  cards.forEach((card) => {
+    card.removeEventListener("click", clickHandler);
+  });
+}
+
+function enableCardClicks() {
+  cards.forEach((card) => {
+    card.addEventListener("click", clickHandler);
+  });
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleArray(cards);
+
+cards.forEach((card) => {
+  imageWrapper.appendChild(card);
+});
+
+function clickHandler() {
+  if (flippedCards.length < 2 && !isChecking) {
+    const card = this;
+    card.classList.add("flipped");
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) {
+      disableCardClicks();
+      setTimeout(checkMatch, 800);
+    }
+  }
+}
+
+function checkMatch() {
+  const [card1, card2] = flippedCards;
+  isChecking = true;
+
+  if (card1.innerHTML === card2.innerHTML) {
+    card1.removeEventListener("click", clickHandler);
+    card2.removeEventListener("click", clickHandler);
+    score += 1;
+    scoreElem.innerHTML = score;
+
+    if (score === imagesArray.length) {
+      const modal = document.getElementById("myModal");
+      modal.style.display = "block";
+
+      const closeButton = document.querySelector(".close");
+      closeButton.addEventListener("click", () => {
+        modal.style.display = "none";
+      });
+    }
+
+    flippedCards = [];
+    isChecking = false;
+    enableCardClicks();
+  } else {
+    setTimeout(() => {
+      card1.classList.remove("flipped");
+      card2.classList.remove("flipped");
+      flippedCards = [];
+      isChecking = false;
+      enableCardClicks();
+    }, 800);
+  }
+}
+
+cards.forEach((card) => {
+  setTimeout(() => {
+    card.classList.add("flipped");
+  }, 1000);
+  setTimeout(() => {
+    card.classList.remove("flipped");
+  }, 4000);
+});
